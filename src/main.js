61405a1426968c655012bb6cb076b6c7e0202a28
Nick Strayer <nick@nicks-mbp.dhcp.mc.vanderbilt.edu>
2017-06-30 11:13:24 -0500

    skeleton up

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const d3 = require('d3');
+
+// const {
+//   simplifyData,
+//   appendSVG,
+//   makeScales,
+//   makeLine,
+//   makeClip,
+//   testImport,
+//   drawAxes,
+//   dragCanvas,
+//   makeUserData,
+//   clamp,
+//   addToClosest,
+//   drawStartValue
+// } = require('./helpers');
+
+
+
+module.exports = drawIt;
+// const ourChart = new drawIt(params);
